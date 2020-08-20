8a94df0560c7eac292e75378faddaf57fd31960b
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 22:29:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,10 +1,10 @@
 def reverse_each_word(text)
   sentence = []
   text.split.each do |word|
-    puts sentence.push(word.reverse).inspect
+    sentence.push(word.reverse)
   end
   sentence
 end
 
 string = "Hello, this is a test"
-reverse_each_word(string)
+puts reverse_each_word(string).inspect
