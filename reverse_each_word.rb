b4682ca3c026e3ae0079db538a417a71b980c1c6
modern-class-9458 <alexander.clamor@gmail.com>
2020-08-20 22:27:53 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,10 +2,10 @@ def reverse_each_word(text)
   sentence = []
   text.split.each do |word|
     new_w = word.reverse
-    puts sentence.push(new_w)
+    puts sentence.push(new_w).inspect
   end
   sentence
 end
 
 string = "Hello, this is a test"
-reverse_each_word(string).inspect
+reverse_each_word(string)
